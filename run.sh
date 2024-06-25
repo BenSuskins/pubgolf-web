99bddec34bab9f26305ecaa4d4ba2ded71923ea1
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-25 22:22:50 +0100

    Fix

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker stop pubgolf-ui
+
 docker build -t pubgolf-ui .
 
-docker run -d -p 3001:3000 --name pubgolf pubgolf-ui
+docker run -d -p 3001:3000 --name pubgolf-ui pubgolf-ui
