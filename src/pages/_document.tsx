8e83f70c1beb4b596e113018f51adfade2bd306c
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-25 23:27:49 +0100

    icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
+        <meta name="apple-mobile-web-app-title" content="Pub Golf"/>
+      </Head>
       <body>
         <Main />
         <NextScript />
