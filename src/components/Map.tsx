49334a0bc85de43a69f97c60be7c7414384cee87
Ben Suskins <suskinsdevelopment@gmail.com>
2024-06-29 18:58:28 +0100

    Fix map to use an actually walkable route

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, Polyline } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import * as L from 'leaflet';
+import { walkingRoute } from '@/utils/constants';
 
 // Fix leaflet's default icon issue with Next.js
 delete (L.Icon.Default.prototype as any)._getIconUrl;
@@ -11,14 +12,14 @@ L.Icon.Default.mergeOptions({
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const MapComponent: FC<MapComponentProps> = ({ drinks, polylinePositions }) => {
+const MapComponent: FC<{ drinks: { pub: string, lat: number, lng: number }[] }> = ({ drinks }) => {
     return (
         <MapContainer center={[51.53877, -0.04521]} zoom={14} style={{ width: '100%', height: '100%' }}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-            <Polyline positions={polylinePositions} color="blue" />
+            <Polyline positions={walkingRoute} color="blue" />
             {drinks.map((drink, index) => (
                 <Marker key={index} position={[drink.lat, drink.lng]}>
                     <Popup>{drink.pub}</Popup>
