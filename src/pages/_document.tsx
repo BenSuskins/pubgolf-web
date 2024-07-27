3ae42ba16ad12b68fe65623a3e3cab18e47be6a6
Ben Suskins <suskinsdevelopment@gmail.com>
2024-07-27 13:10:04 +0100

    Theme color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
         <meta name="apple-mobile-web-app-capable" content="yes"/>
         <meta name="apple-mobile-web-app-title" content="Pub Golf"/>
+        <meta name="theme-color" content="#2b363b"/>
       </Head>
       <body>
         <Main />
