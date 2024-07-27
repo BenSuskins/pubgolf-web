e299661380b4deb7d8fdc0335a044545526f7ba3
Ben Suskins <suskinsdevelopment@gmail.com>
2024-07-27 13:14:32 +0100

    Fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-title" content="Pub Golf" />
         <meta name="theme-color" content="#2b363b" />
       </Head>
